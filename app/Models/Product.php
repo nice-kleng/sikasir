23ec7b60883d4b2f9bca67c4185cb19fddf1704a
NiceCode9 <nicecode9@gmail.com>
2025-02-27 18:08:34 +0700

    add kategori field to fillable attributes in Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     protected $table = 'products';
-    protected $fillable = ['nama_menu', 'harga', 'foto', 'deskripsi'];
+    protected $fillable = ['nama_menu', 'kategori', 'harga', 'foto', 'deskripsi'];
 
     public function transactionItem()
     {
